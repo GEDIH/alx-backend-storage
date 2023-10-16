28fa0ca2a6c443f829bd34efe06914c428798d29
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2023-10-16 09:10:37 +0300

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,1 @@
-#!/usr/bin/env python3
-""" Module for using PyMongo """
 
-
-def insert_school(mongo_collection, **kwargs):
-    """ Inserts new document in collection based on kwargs """
-    id_obj = mongo_collection.insert_one(kwargs)
-
-    return id_obj.inserted_id
