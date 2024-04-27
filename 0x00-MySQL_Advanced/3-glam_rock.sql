14ac9d4438850a71270c4ab197649535defcbaff
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2024-04-27 20:08:34 +0300

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,1 +1,7 @@
-
+-- Lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT
+band_name, ifnull(split, 2022)-ifnull(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
