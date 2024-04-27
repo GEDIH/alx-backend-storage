fc805019e70d0e85c9a81dc221da7d5bba225673
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2024-04-27 11:13:34 +0300

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,1 @@
--- Create a table 'users' with unique email constraint
--- script can be executed on any database
-CREATE TABLE If NOT EXISTS `users` (  
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
-);
+
