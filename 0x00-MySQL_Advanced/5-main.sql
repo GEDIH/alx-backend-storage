37985940b518ff75c82262af9c8e0d715f547a6e
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2024-04-27 20:14:31 +0300

    5-main.sql

--- 0x00-MySQL_Advanced/5-main.sql
+++ 0x00-MySQL_Advanced/5-main.sql
@@ -1,5 +1,5 @@
 Enter password: 
--- Show users and update (or not) email
+---- Show users and update (or not) email
 SELECT * FROM users;
 
 UPDATE users SET valid_email = 1 WHERE email = "bob@dylan.com";
