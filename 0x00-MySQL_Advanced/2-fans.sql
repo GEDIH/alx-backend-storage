cea6bd01c43da3b28b2769760ff9da79dd3dad70
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2024-04-27 20:05:07 +0300

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,1 @@
--- Rank country origins of bands by the number of fans
--- ordered by the number of (non-unique) fans
-SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
-GROUP BY `origin`
-ORDER BY `nb_fans` DESC;
+
